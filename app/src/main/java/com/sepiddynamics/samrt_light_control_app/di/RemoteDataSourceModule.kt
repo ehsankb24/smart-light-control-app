7f1fcf51e5ccfe3c2d9f85a4dd45643adc8f174c
ehsan zarghalami <ehsan.zarghalami@gmail.com>
2025-04-29 00:33:55 +0330

    implement MqttClient di providers

---
+++ app/src/main/java/com/sepiddynamics/samrt_light_control_app/di/RemoteDataSourceModule.kt
@@ -0,0 +1,38 @@
+package com.sepiddynamics.samrt_light_control_app.di
+
+import android.content.Context
+import com.sepiddynamics.samrt_light_control_app.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import org.eclipse.paho.client.mqttv3.MqttClient
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
+
+@InstallIn(SingletonComponent::class)
+@Module
+class RemoteDataSourceModule {
+
+    @ClientId
+    @Provides
+    fun provideClientId(
+        @ApplicationContext context: Context
+    ): String = context.getString(R.string.client_id)
+
+    @ServerUrl
+    @Provides
+    fun provideServerAddress(
+        @ApplicationContext context: Context
+    ): String = context.getString(R.string.server_address)
+
+    @Provides
+    fun provideMqttClient(
+        @ServerUrl serverUrl: String,
+        @ClientId clientId: String
+    ) = MqttClient(
+        serverUrl,
+        clientId,
+        MemoryPersistence()
+    )
+}
