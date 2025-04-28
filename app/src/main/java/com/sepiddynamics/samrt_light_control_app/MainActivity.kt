b1bdb2de0014a60535467a30b0b12d4289d0512c
ehsan zarghalami <ehsan.zarghalami@gmail.com>
2025-04-29 00:08:18 +0330

    implement ui

--- app/src/main/java/com/sepiddynamics/samrt_light_control_app/MainActivity.kt
+++ app/src/main/java/com/sepiddynamics/samrt_light_control_app/MainActivity.kt
@@ -4,44 +4,148 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.sepiddynamics.samrt_light_control_app.ui.theme.SamrtlightcontrolappTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SamrtlightcontrolappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val viewState by viewModel.viewState.collectAsStateWithLifecycle()
+                MainLayout(viewState)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun MainLayout(
+    viewState: MainViewState,
+) {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        val modifier = Modifier
+            .padding(innerPadding)
+            .padding(16.dp)
+        when (viewState) {
+            is MainViewState.Connected -> ConnectedLayout(
+                modifier = modifier,
+                state = viewState
+            )
+
+            is MainViewState.Failed -> FailedLayout(modifier, onRetry = viewState.retry)
+            MainViewState.Loading -> LoadingLayout(modifier)
+        }
+    }
+}
+
+@Composable
+private fun LoadingLayout(modifier: Modifier = Modifier) = Box(modifier = modifier.fillMaxSize()) {
+    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+}
+
+@Composable
+private fun FailedLayout(
+    modifier: Modifier = Modifier,
+    onRetry: () -> Unit
+) = Column(
+    modifier = modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center
+) {
+    Text("error")
+    Button(onClick = onRetry) {
+        Text("retry")
+    }
+}
+
+@Composable
+private fun ConnectedLayout(
+    modifier: Modifier = Modifier,
+    state: MainViewState.Connected
+) {
+    Column(
+        modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                state.stateTitle
+            )
+            Checkbox(
+                checked = state.isOn,
+                onCheckedChange = state.onCheckedChange
+            )
+        }
+        Slider(
+            value = state.progressValue,
+            onValueChange = state.onValueChange,
+            onValueChangeFinished = state.onValueChangeFinished,
+            valueRange = 0f..100f
+        )
+        Text(text = state.progressLabel)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+private fun MainLayoutLoadingPreview() {
     SamrtlightcontrolappTheme {
-        Greeting("Android")
+        MainLayout(
+            viewState = MainViewState.Loading
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MainLayoutFailedPreview() {
+    SamrtlightcontrolappTheme {
+        MainLayout(
+            viewState = MainViewState.Failed {}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MainLayoutConnectPreview() {
+    SamrtlightcontrolappTheme {
+        MainLayout(
+            viewState = MainViewState.Connected(
+                isOn = true,
+                progressValue = 10f,
+                onValueChange = {},
+                onCheckedChange = {},
+                onValueChangeFinished = {}
+            )
+        )
     }
 }
